5ad27c089372630cbf4c90bf3fd92137e855573b
LeonardoLobas <leonardolobas2703@gmail.com>
2025-05-06 10:30:56 -0300

    melhorar a clareza do código no formulário de cadastro

--- scripts/formStorage.js
+++ scripts/formStorage.js
@@ -6,19 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const inputs = form.querySelectorAll('input[name]:not([type="file"])');
 
-        // Salvar CADA campo separadamente no localStorage
         inputs.forEach((input) => {
             localStorage.setItem(input.name, input.value);
         });
 
         alert("Cadastro finalizado com sucesso!");
 
-        // Limpar apenas os valores dos inputs, não o localStorage
         inputs.forEach((input) => {
             input.value = "";
         });
 
-        // Voltar para o primeiro step
         if (typeof resetStepsToFirst === "function") {
             resetStepsToFirst();
         }
