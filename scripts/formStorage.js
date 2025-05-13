539148447e588a4b4c8be48181e7811fb877ad91
LeonardoLobas <leonardolobas2703@gmail.com>
2025-05-13 08:14:45 -0300

    adiciona validação de imagem no cadastro e permite envio com a tecla Enter

--- scripts/formStorage.js
+++ scripts/formStorage.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
+        const previewImage = document.getElementById("preview-image");
+        const currentSrc = previewImage.getAttribute("src");
+        const isDefault = currentSrc.includes("default-avatar.png");
+
+        if (isDefault) {
+            alert("Por favor, selecione uma imagem antes de continuar.");
+            return; // Impede de finalizar o cadastro
+        }
+
         const inputs = form.querySelectorAll("input[name]");
 
         inputs.forEach((input) => {
